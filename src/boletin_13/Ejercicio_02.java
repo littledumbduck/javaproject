aa9cc0082fb5a98a4e7090d64ec0f469e5cbf004
littledumbduck <ale0014@alu.medac.es>
2026-01-15 08:34:05 +0100

    15/01

--- src/boletin_13/Ejercicio_02.java
+++ src/boletin_13/Ejercicio_02.java
@@ -1,9 +1,24 @@
 package boletin_13;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.Random;
 
 public class Ejercicio_02 {
     public static void main(String[] args) {
         Random rand = new Random();
+        int num = rand.nextInt(100) % 2;
+
+        System.out.println("Numero: " + num);
+
+        try {
+            if(num == 0){
+                throw new Exception("El número es par");
+            } else if (num == 1){
+                throw new Exception("El número es impar");
+            }
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+
     }
 }
